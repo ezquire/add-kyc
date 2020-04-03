17528eb98045e174f9df4c0dd8db94376ec2e07a
tyler-ezquire <tyler.gearing@synapsefi.com>
2020-04-02 17:09:53 -0700

    working prototype

--- add_kyc.py
+++ add_kyc.py
@@ -20,79 +20,80 @@ print("*************************************************************************
 #'5e7bedbe9f1eef0092a3b3f9'
 
 user_id = input("User ID: ")
-# number_of_other_documents = input("Number of 'Other' Documents: ")
-print("\nEnter y/n for the following KYC options (these documents are the required KYC for the PLATFORM under v3 controls):")
+# # number_of_other_documents = input("Number of 'Other' Documents: ")
+# print("\nEnter y/n for the following KYC options (these documents are the required KYC for the PLATFORM under v3 controls)\n")
+# aoi_doc = input("AOI_DOC: ").lower()
+# bylaws_doc = input("BYLAWS_DOC: ").lower()
+# print(aoi_doc, bylaws_doc)
+user = client.get_user(
+    user_id, ip=ip, fingerprint=fingerprint, full_dehydrate=True)
 
-# user = client.get_user(
-#     user_id, ip=ip, fingerprint=fingerprint, full_dehydrate=True)
+oauth_body = {
+    "refresh_token": user.body["refresh_token"],
+    "scope": [
+        "USER|PATCH",
+        "USER|GET",
+        "NODES|POST",
+        "NODES|GET",
+        "NODE|GET",
+        "NODE|PATCH",
+        "NODE|DELETE",
+        "TRANS|POST",
+        "TRANS|GET",
+        "TRAN|GET",
+        "TRAN|PATCH",
+        "TRAN|DELETE",
+        "SUBNETS|GET",
+        "SUBNETS|POST",
+        "SUBNET|GET",
+        "SUBNET|PATCH",
+        "STATEMENTS|GET",
+        "STATEMENT|GET",
+        "STATEMENTS|POST"
+    ]
+}
 
-# oauth_body = {
-#     "refresh_token": user.body["refresh_token"],
-#     "scope": [
-#         "USER|PATCH",
-#         "USER|GET",
-#         "NODES|POST",
-#         "NODES|GET",
-#         "NODE|GET",
-#         "NODE|PATCH",
-#         "NODE|DELETE",
-#         "TRANS|POST",
-#         "TRANS|GET",
-#         "TRAN|GET",
-#         "TRAN|PATCH",
-#         "TRAN|DELETE",
-#         "SUBNETS|GET",
-#         "SUBNETS|POST",
-#         "SUBNET|GET",
-#         "SUBNET|PATCH",
-#         "STATEMENTS|GET",
-#         "STATEMENT|GET",
-#         "STATEMENTS|POST"
-#     ]
-# }
+user.oauth(oauth_body)
 
-# user.oauth(oauth_body)
+business_response = user.update_info(business_docs)
+owner_response = user.update_info(beneficial_owner_docs)
 
-# business_response = user.update_info(business_docs)
-# owner_response = user.update_info(beneficial_owner_docs)
+ubo_docs = {
+    "entity_info": {
+        "cryptocurrency": True,
+        "msb": {
+            "federal": True,
+            "states": [
+                "AL"
+            ]
+        },
+        "public_company": False,
+        "majority_owned_by_listed": False,
+        "registered_SEC": False,
+        "regulated_financial": False,
+        "gambling": False,
+        "document_id": owner_response["documents"][0]["id"]
+    },
+    "signer": {
+        "document_id": owner_response["documents"][1]["id"],
+        "relationship_to_entity": "CEO"
+    },
+    "compliance_contact": {
+        "document_id": owner_response["documents"][1]["id"],
+        "relationship_to_entity": "CEO"
+    },
+    "primary_controlling_contact": {
+        "document_id": owner_response["documents"][1]["id"],
+        "relationship_to_entity": "CEO"
+    },
+    "owners": [
+        {
+            "document_id": owner_response["documents"][1]["id"],
+            "title": "CEO",
+            "ownership": 95
+        }
+    ]
+}
 
-# ubo_docs = {
-#     "entity_info": {
-#         "cryptocurrency": True,
-#         "msb": {
-#             "federal": True,
-#             "states": [
-#                 "AL"
-#             ]
-#         },
-#         "public_company": False,
-#         "majority_owned_by_listed": False,
-#         "registered_SEC": False,
-#         "regulated_financial": False,
-#         "gambling": False,
-#         "document_id": owner_response["documents"][0]["id"]
-#     },
-#     "signer": {
-#         "document_id": owner_response["documents"][1]["id"],
-#         "relationship_to_entity": "CEO"
-#     },
-#     "compliance_contact": {
-#         "document_id": owner_response["documents"][1]["id"],
-#         "relationship_to_entity": "CEO"
-#     },
-#     "primary_controlling_contact": {
-#         "document_id": owner_response["documents"][1]["id"],
-#         "relationship_to_entity": "CEO"
-#     },
-#     "owners": [
-#         {
-#             "document_id": owner_response["documents"][1]["id"],
-#             "title": "CEO",
-#             "ownership": 95
-#         }
-#     ]
-# }
-
-# response = user.create_ubo(ubo_docs)
-# print(response)
+response = user.create_ubo(ubo_docs)
 
